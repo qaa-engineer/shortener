856f6e77922148116768122e4ba5b69a1a7dd8b0
Максим Алексеев <msalekseev@sbercloud.ru>
2023-12-26 13:33:32 +0300

    iter2

--- go.mod
+++ go.mod
@@ -6,3 +6,4 @@ require (
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/itchyny/base58-go v0.2.1
 )
+
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,6 +1,14 @@
 package storage
 
+import "sync"
+
+type URLRepository interface {
+	AddURL(shortLink, originalLink string)
+	GetURL(shortLink string) (string, bool)
+}
+
 type URLStorage struct {
+	mu   sync.Mutex
 	Urls map[string]string
 }
 
@@ -8,4 +16,20 @@ func NewURLStorage() *URLStorage {
 	return &URLStorage{
 		Urls: make(map[string]string),
 	}
+}
+
+func (s *URLStorage) AddURL(shortLink, originalLink string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.Urls[shortLink] = originalLink
+}
+
+func (s *URLStorage) GetURL(shortLink string) (string, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	url, ok := s.Urls[shortLink]
+
+	return url, ok
 }
