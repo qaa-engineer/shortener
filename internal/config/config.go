c7ab2cb1c631d896c89a1ee936d34010dfc84601
Максим Алексеев <msalekseev@sbercloud.ru>
2024-01-15 11:05:38 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v6"
+)
+
 type Configuration struct {
 	Address         string
 	BaseResponseURL string
@@ -15,4 +21,22 @@ func NewConfiguration() *Configuration {
 		Address:         "localhost:8080",
 		BaseResponseURL: "http://localhost:8080/",
 	}
+}
+
+func (c *Configuration) ParseConfiguration() {
+
+	cfg := EnvConfiguration{}
+	err := env.Parse(&cfg)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	if len(cfg.ServerAddress) > 0 {
+		c.Address = cfg.ServerAddress
+	}
+
+	if len(cfg.BaseURL) > 0 {
+		c.BaseResponseURL = cfg.BaseURL
+	}
+
 }
